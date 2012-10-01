04137429f44954bea605d9c2e146139c202bbb6c
Samuel Christie <emailium@gmail.com>
2012-09-30 22:42:43 -0400

    Begin python bot

---
+++ kingbot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+import sqlalchemy
+import sys
+
+### create new accounts
+#connect to kingsage
+#generate name
+#register account
+#activate account on mailinator
+#create town in random quadrant
